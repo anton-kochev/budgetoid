1471a60719902d33aac73f05e8c81df597f92172
Anton <anton-kochev@users.noreply.github.com>
2024-01-25 21:12:49 +0100

    Added vuetify

--- ClientApp/vue-budgetoid/src/main.ts
+++ ClientApp/vue-budgetoid/src/main.ts
@@ -7,12 +7,23 @@ import vue3GoogleLogin from 'vue3-google-login';
 import App from './App.vue';
 import router from './router';
 
+// Vuetify
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import 'vuetify/styles';
+
 const app = createApp(App);
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 app.use(vue3GoogleLogin, {
-  clientId: '',
+  clientId: 'YOUR_CLIENT_ID.apps.googleusercontent.com',
 });
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 
 app.mount('#app');
