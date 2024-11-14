dabc9ff2da49c4518c0412da39e085695ecbe5c7
Anton <anton-kochev@users.noreply.github.com>
2024-11-14 13:56:15 +0100

    Add light and dark themes; design the Welcome page
    #5

--- ClientApp/angular-budgetoid/src/app/welcome/welcome.component.ts
+++ ClientApp/angular-budgetoid/src/app/welcome/welcome.component.ts
@@ -1,8 +1,44 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  template: ` <h1>Welcome to the Welcome Module!</h1> `,
+  styles: [
+    `
+      :host {
+        display: grid;
+        grid-template-rows: auto 1fr auto;
+        padding: 2rem;
+      }
+
+      .welcome {
+        &__about {
+          flex-grow: 1;
+          margin-bottom: 2rem;
+          text-align: center;
+        }
+        &__title {
+          font-size: 4rem;
+          font-weight: 400;
+          text-align: center;
+          width: 100%;
+        }
+        &__login {
+          text-transform: uppercase;
+          justify-self: center;
+        }
+      }
+    `,
+  ],
+  template: `
+    <h1 class="welcome__title">BUDGETOID</h1>
+    <p class="welcome__about">
+      Welcome to Budgetoid, the budgeting app that helps you manage your
+      finances!
+    </p>
+    <button mat-stroked-button class="welcome__login">Login</button>
+  `,
+  imports: [MatButtonModule],
 })
 export class WelcomeComponent {}
